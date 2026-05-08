004b57c238afdba9d59af0b704c8a1da229bc921
James Kalisz <james.kalisz@tdsb.on.ca>
2026-05-08 09:30:22 -0400

    update notes

--- src/main/java/Notes.java
+++ src/main/java/Notes.java
@@ -2,5 +2,16 @@
 class Notes {
 	public static void main(String[] args) {
 
+		RectPrism r = new RectPrism(2, 4, 5);
+		System.out.println(r.getLength());
+		System.out.println(r.getThisLength());
+		System.out.println(r.getSuperLength());
+
+
+		Rectangle rectangle = new Rectangle(0, 0);
+		RectPrism rectPrism = new RectPrism(2, 2, 2);
+
+		System.out.println(rectangle.area());
+		System.out.println(rectPrism.area());
 	}
 }
